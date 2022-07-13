ade3ff7adfd5499781ef1b1fa8e411144bb4d175
bennoholik <bennoholik@gmail.com>
2022-07-13 20:47:20 +0900

    임의의 쉼터 리스트 프론트 구현, 임의의 현주소 좌표 구현

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 
-from controller import userController, mainController, weatherController
+from controller import userController, mainController, weatherController,searchController
 
 app = Flask(__name__)
 SECRET_KEY = 'SPARTA'
 userController.start(app)
 mainController.start(app)
 weatherController.start(app)
+searchController.start(app)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
