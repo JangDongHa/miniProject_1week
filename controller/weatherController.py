704a913a669ce51b66e908a209622d1d0d3d00da
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2022-07-13 20:15:31 +0900

    지역별 날씨정보 구현완료

--- controller/weatherController.py
+++ controller/weatherController.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify
+
+from model.localDto import localDTO
 from service import userService, ipService, tempService
 from service.tempService import check_weather
 
@@ -21,4 +23,9 @@ def start(app, data=''):
         SKY, PTY, T1H = tempService.get_SKY_PTY_T1H_in_short_term_forecast()
         print(T1H)
         return render_template('weather.html', SKY=SKY, PTY=PTY, T1H=T1H, check_weather=check_weather)
+
+    @app.route('/weather/<local>', methods=['GET'])
+    def get_local_weather(local):
+        SKY, PTY, T1H = tempService.get_SKY_PTY_T1H_in_short_term_forecast(localDTO['cords'][local][0], localDTO['cords'][local][1])
+        return render_template('weather.html', SKY=SKY, PTY=PTY, T1H=T1H, check_weather=check_weather, address=localDTO['address'][local])
 
